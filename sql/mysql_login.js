2f01d1a01ebb0454691eea85e72381e1e48b859f
tom_ssy <479151277@qq.com>
2017-04-26 10:55:14 +0800

    mysql使用连接池

--- sql/mysql_login.js
+++ sql/mysql_login.js
@@ -2,13 +2,13 @@
  * Created by Administrator on 2017/4/2 0002.
  */
 
-let client = require('mysql').createConnection({
+let client = require('mysql').createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database:'school_manager'
 });
-client.connect();
+// client.connect();
 
 function makePromise(commend) {
     return new Promise((resolve, reject)=> {
@@ -21,6 +21,30 @@ function makePromise(commend) {
         });
     });
 }
+
+let query=function(sql){
+    return new Promise((resolve, reject)=> {
+        client.getConnection(function (err, conn) {
+            if (err) {
+                callback(err, null, null);
+            } else {
+
+                conn.query(sql, function (qerr, vals, fields) {
+                    //释放连接
+                    conn.release();
+                    //事件驱动回调
+                    if (err == null) {
+                        resolve(vals);
+                    } else {
+                        reject(qerr);
+                    }
+                    // callback(qerr,vals,fields);
+                });
+            }
+        })
+    })
+};
+exports.query=query;
 
 exports.client = client;
 exports.makePromise = makePromise;
--- sql/teacher.js
+++ sql/teacher.js
@@ -4,11 +4,13 @@
 let client = require('./mysql_login').client;
 let co = require('co');
 let makePromise = require('./mysql_login').makePromise;
+let query = require('./mysql_login').query;
 
 // var await = require('asyncawait/await');
 let teacher_data = function (res, username, profession) {
+    
     let classExist = function () {
-                let getClassList = makePromise(`SELECT * from class WHERE class_name="1508A"`);
+                let getClassList = query(`SELECT * from class WHERE class_name="1508A"`);
                 return getClassList
     };
 
@@ -16,26 +18,29 @@ let teacher_data = function (res, username, profession) {
         if(hasClass.length==0){
             res.send({status:201,des:"用户名不存在"});
         }else{
-            let getStudentList = makePromise(`SELECT s.id,s.birthday,s.id_card,s.length_of_schooling,s.name,s.now_has_course,s.score,
+
+                let getStudentList = query(`SELECT s.id,s.birthday,s.id_card,s.length_of_schooling,s.name,s.now_has_course,s.score,
          s.sex,s.student_status,s.time_of_come_school,c.class_name,ma.name FROM student s 
 LEFT JOIN class c ON s.class_id=c.id
 LEFT JOIN market ma ON s.market_id=ma.id`);
-            let getClassList = makePromise(`SELECT id,class_name from class`);
-            let rewards = makePromise(`SELECT * FROM reward_and_punishment`);
-            let courseList = makePromise(`SELECT * FROM course`);
-            let thisTeachingCycle = makePromise(`SELECT cycle_name,start_time,end_time FROM teaching_cycle WHERE type=1`);
-            let theClassData=makePromise(`SELECT c.id,c.class_name,he.name,co.course_name,c.cScore,c.status,c.profession FROM class c 
+                let getClassList = query(`SELECT id,class_name from class`);
+                let rewards = query(`SELECT * FROM reward_and_punishment`);
+                let courseList = query(`SELECT * FROM course`);
+                let thisTeachingCycle = query(`SELECT cycle_name,start_time,end_time FROM teaching_cycle WHERE type=1`);
+                let theClassData = query(`SELECT c.id,c.class_name,he.name,co.course_name,c.cScore,c.status,c.profession FROM class c 
 left join headmaster he on c.headMaster_id=he.id
 left join teacher te on c.teacher_id=te.teacher
 left join course co on c.course_id=co.id
 where c.class_name="1508A" and c.profession=0`);
-            let students=makePromise(`SELECT stu.id,stu.name,stu.sex,stu.id_card,stu.birthday,stu.time_of_come_school,cl.class_name,ma.name market
+                let students = query(`SELECT stu.id,stu.name,stu.sex,stu.id_card,stu.birthday,stu.time_of_come_school,cl.class_name,ma.name market
 ,stu.length_of_schooling,stu.score,stu.now_has_course,stu.student_status,stu.no 
 FROM student stu
 left join class cl on cl.id=stu.class_id
 left join market ma on stu.market_id=ma.id`);
-            return Promise.all([getStudentList, getClassList, rewards, courseList, thisTeachingCycle,theClassData,students]);
-        }
+                return Promise.all([getStudentList, getClassList, rewards, courseList, thisTeachingCycle, theClassData, students]);
+            }
+            
+
 
     };
 
@@ -48,7 +53,7 @@ left join market ma on stu.market_id=ma.id`);
 
     //数据回调发送
     co(teacherGenerator).then(function (json) {
-        console.log(123123123);
+      
         let teacher_data = {
             status: 200
         };
