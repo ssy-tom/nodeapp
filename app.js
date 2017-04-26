e590221c44fc2b45da4068c60a520515f8cb75a8
tom_ssy <479151277@qq.com>
2017-04-26 13:28:55 +0800

    access *

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ timeManager.timeUpdate();
 var app = express();
 console.log(__dirname);
 // view engine setup
-// app.all('*', function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-//   res.header("X-Powered-By", ' 3.2.1');
-//   res.header("Content-Type", "application/json;charset=utf-8");
-//   next();
-// });
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
